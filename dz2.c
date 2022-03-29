08a46e4e6f94b0be1b5561d0e7071865ba28a6a6
SvirkoA <94311402+SvirkoA@users.noreply.github.com>
2022-03-29 11:06:24 +0300

    Update dz2.c

--- dz2.c
+++ dz2.c
@@ -1,90 +1,41 @@
-#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#include <stdlib.h>
+#include <math.h>
 
 int main()
 {
-    FILE *input, *output, *output1;
-    char a, s[30][30];
-    int s2[30][30];
-    int i = 0, j = 0, m = 0, n = 0, count = 0, b = 0;
-
-    input = fopen("input.txt", "r"); 
-    while (!feof(input)) {
-        fscanf(input, "%c", &a);
-        if (a == '1' || a == '0') { 
-            s[i][j] = a;
-            j++;
-        }
-        if (a == '\n') { 
-            i++;
-            j = 0;
-        }
+  int s1[1001], s2[1001], i, ost, summ, degree, count;
+  count = 0;
+  i = 0;
+  while ((scanf("%d", &s1[i])) != EOF) {
+    i++;
+  }
+  printf("\n");
+  printf("Введенный массив:\n");
+  for (int j = 0; j < i; j++) {
+    printf("%d: ", j + 1);
+    printf("%d\n", s1[j]);
+    degree = 0;
+    while (s1[j] != 0) {
+      ost = s1[j] % 10;
+      s1[j] = s1[j] / 10;
+      summ += ost * pow(2, degree);
+      degree++;
+      if (ost != 0 && ost != 1) {
+        count++;
+      }
     }
-    for (int n = 0; n < j; n++) { 
-        count = 0;
-        for (int m = 0; m <= i; m++) {
-            if (s[m][n] == '1' && count == 0) {
-                count++;
-                b = m;
-            }
-            else if (s[m][n] == '1') {
-                count++;
-                if (count == 2) { 
-                    s2[b][m] = '1';
-                    s2[m][b] = '1';
-                }
-            }
-        }
+    s2[j] = summ;
+    summ = 0;
+  }
+  printf("\n");
+  if (count != 0) {
+    printf("Вы неправильно ввели двоичные числа\n");
+  }
+  else {
+    printf("Полученный массив:\n");
+    for (int j = 0; j < i; j++) {
+      printf("%d: ", j + 1);
+      printf("%d\n", s2[j]);
     }
-    output1 = fopen("output1.txt", "w");
-    for (int m = 0; m <= i; m++) {
-        fprintf(output1, "%d: ", m + 1);
-        count = 0;
-        for (int n = 0; n < j; n++) {
-            if (s2[m][n] == '1') {
-                fprintf(output1, "%d ", n + 1);
-                count++;
-            }
-        }
-        fprintf(output1, "\n");
-    }
-    fclose(output1);
-    output = fopen("output.gv", "w"); 
-    fprintf(output, "graph G {\n");
-    fprintf(output, "node [shape = circle];\n");
-    for (int n = 0; n < j; n++) {
-        count = 0;
-        for (int m = 0; m <= i; m++) {
-            if (s[m][n] == '1' && count == 0) {
-                count++;
-                b = m + 1;
-            }
-            else if (s[m][n] == '1') {
-                count++;
-                if (count == 2) {
-                    fprintf(output, "%d -- %d\n", b, m + 1);
-                }
-            }
-        }
-        if (count == 1) {
-            fprintf(output, "%d -- %d\n", b, b);
-        }
-    }
-    for (int m = 0; m <= i; m++) { 
-        count = 0;
-        for (int n = 0; n < j; n++) {
-            if (s[m][n] == '1') {
-                count++;
-            }
-        }
-        if (count == 0) { 
-            fprintf(output, "%d\n", m + 1);
-        }
-    }
-    fprintf(output, "}");
-    fclose(output);
-    system("dot output.gv -Tpng -o graph.png");
-    system("graph.png");
-    return 0;
+  }
 }
